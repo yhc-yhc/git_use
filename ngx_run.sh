07cad9881fd02cd40f898506097d1a879fd0d586
josh <josh.zhu@pictureworks.biz>
2017-03-28 16:28:17 +0800

    josh

--- ngx_run.sh
+++ ngx_run.sh
@@ -2,14 +2,15 @@
 cd ~/work
 echo `pwd`
 
-if [ -d git_use ] then
+if [ -d git_use ] 
+then
 	echo 'update ...'
 	cd git_use;
 	git pull origin master;
 	cd ../;
 else 
 	echo 'clone ...';
-	# git clone https://github.com/yhc-yhc/git_use.git;
+	git clone https://github.com/yhc-yhc/git_use.git;
 fi
 
 cp -r git_use/ngx_conf conf/
