b5102dbe2359792df4ad5a66c1c226dc71389350
josh <josh.zhu@pictureworks.biz>
2017-03-28 16:17:18 +0800

    josh

--- ngx_run.sh
+++ ngx_run.sh
@@ -1,10 +1,12 @@
 
 cd ~/work
 if [ -d git_use ] then
+	echo 'update ...'
 	cd git_use;
 	git pull origin master;
-	cd ../
-else
+	cd ../;
+else 
+	echo 'clone ...'
 	git clone https://github.com/yhc-yhc/git_use.git
 fi
 cp -r git_use/ngx_conf conf/
