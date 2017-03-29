4ae57a99290f341c1798f71d91d95f8f3cecd6e9
josh <josh.zhu@pictureworks.biz>
2017-03-29 13:42:32 +0800

    josh

--- ngx_conf/ngx_start.sh
+++ ngx_conf/ngx_start.sh
@@ -4,5 +4,4 @@ docker stop nginx > /dev/null 2>&1
 echo 'remove nginx container'
 docker rm nginx > /dev/null 2>&1
 echo 'start nginx container'
-. run_box.sh
-box image=nginx p=80 l=localhost:demo v=~/logs/nginx:/var/log/nginx v=`pwd`/local:/www v=~/conf/ngx_conf:/etc/nginx/conf.d
+./run_box.sh image=nginx p=80 l=localhost:demo v=~/logs/nginx:/var/log/nginx v=`pwd`/local:/www v=~/conf/ngx_conf:/etc/nginx/conf.d
--- ngx_conf/run_box.sh
+++ ngx_conf/run_box.sh
@@ -1,92 +1,91 @@
-box(){
-	command="docker run -d --restart=always --privileged=true"
-	command="${command} -v /etc/localtime:/etc/localtime"
 
-	confirm(){
-		echo 'confirm'
-		read -p 'plase write the image name you want to run:' image
-		name=$image
-		command="${command} ${v_str} ${link_str} ${port_str} --name $name ${image}"
-	}
+command="docker run -d --restart=always --privileged=true"
+command="${command} -v /etc/localtime:/etc/localtime"
 
-	fomat_params(){
-		name='';
-		image='';
-		p=();
-		v=();
-		l=()
-		for item in $@
-		do
-		    IFS="="
-		    item=($item)
-		    case ${item[0]} in
-		    	name)
-					name=${item[1]}
-				;;
-				image)
-					image=${item[1]}
-				;;
-				p)
-					len=${#p[@]}
-					p[len]=${item[1]}
-				;;
-				v)
-					len=${#v[@]}
-					v[len]=${item[1]}
-				;;
-				l)
-					len=${#l[@]}
-					l[len]=${item[1]}
-				;;
-		    esac
-		done
-		echo name: $name;
-		echo port: ${p[@]};
-		echo v: ${v[@]};
-		echo link: ${l[@]};
-		echo image: $image;
+confirm(){
+	echo 'confirm'
+	read -p 'plase write the image name you want to run:' image
+	name=$image
+	command="${command} ${v_str} ${link_str} ${port_str} --name $name ${image}"
+}
 
-		if [ -z image ]
-		then
-			read -p 'plase write the image name you want to run:' image
-		fi
+fomat_params(){
+	name='';
+	image='';
+	p=();
+	v=();
+	l=()
+	for item in $@
+	do
+	    IFS="="
+	    item=($item)
+	    case ${item[0]} in
+	    	name)
+				name=${item[1]}
+			;;
+			image)
+				image=${item[1]}
+			;;
+			p)
+				len=${#p[@]}
+				p[len]=${item[1]}
+			;;
+			v)
+				len=${#v[@]}
+				v[len]=${item[1]}
+			;;
+			l)
+				len=${#l[@]}
+				l[len]=${item[1]}
+			;;
+	    esac
+	done
+	echo name: $name;
+	echo port: ${p[@]};
+	echo v: ${v[@]};
+	echo link: ${l[@]};
+	echo image: $image;
 
-		if [ -z name ]
-		then
-			name=$image
-		fi
-
-		port_str=""
-		for port in ${p[@]}
-		do
-			port_str="${port_str} -p ${port}:${port}"
-		done
-
-		v_str=""
-		for vl in ${v[@]}
-		do
-			v_str="${v_str} -v ${vl}"
-		done
-
-		link_str=""
-		for cl in ${l[@]}
-		do
-			link_str="${link_str} --link ${cl}"
-		done
-		command="${command} ${v_str} ${link_str} ${port_str} --name $name ${image}"
-	}
-
-	if [ $# = 0 ]
+	if [ -z image ]
 	then
-		echo "no param"
-		confirm
-	else
-		fomat_params $@
+		read -p 'plase write the image name you want to run:' image
 	fi
 
-	echo "${command}"
+	if [ -z name ]
+	then
+		name=$image
+	fi
 
-	docker stop ${name}
-	docker rm ${name}
-	command
+	port_str=""
+	for port in ${p[@]}
+	do
+		port_str="${port_str} -p ${port}:${port}"
+	done
+
+	v_str=""
+	for vl in ${v[@]}
+	do
+		v_str="${v_str} -v ${vl}"
+	done
+
+	link_str=""
+	for cl in ${l[@]}
+	do
+		link_str="${link_str} --link ${cl}"
+	done
+	command="${command} ${v_str} ${link_str} ${port_str} --name $name ${image}"
 }
+
+if [ $# = 0 ]
+then
+	echo "no param"
+	confirm
+else
+	fomat_params $@
+fi
+
+echo "${command}"
+
+docker stop ${name}
+docker rm ${name}
+command
