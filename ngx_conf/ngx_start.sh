cf74c656cf54aede0e69fbd05dca5f2c99de78d7
josh <josh.zhu@pictureworks.biz>
2017-03-29 10:26:23 +0800

    josh

--- ngx_conf/ngx_start.sh
+++ ngx_conf/ngx_start.sh
@@ -4,4 +4,4 @@ docker stop nginx > /dev/null 2>&1
 echo 'remove nginx container'
 docker rm nginx > /dev/null 2>&1
 echo 'start nginx container'
-docker run --link demo:localhost --link jenkins:localhost -v /etc/localtime:/etc/localtime -v ~/logs/nginx:/var/log/nginx -v `pwd`/frontend:/www -v ~/conf/ngx_conf:/etc/nginx/conf.d -p 80:80 -p 8000:8000 -p 3000:3000 -p 8080:8080 -p 8090:8090 -d --name nginx --restart=always --privileged=true nginx
+docker run --link demo:localhost --link jenkins:localhost -v /etc/localtime:/etc/localtime -v ~/logs/nginx:/var/log/nginx -v `pwd`/frontend:/www -v ~/conf/ngx_conf:/etc/nginx/conf.d -p 80:80 -p 8000:8000 -p 3000:3000 -p 8090:8090 -d --name nginx --restart=always --privileged=true nginx
